6327868d6de332d323c715ae6eb97369c3a7b539
jose-candido <jose.pedro.martins.candido@ist.utl.pt>
2023-10-17 10:26:18 +0100

    Fix menus

--- js/menuOpen.js
+++ js/menuOpen.js
@@ -1,22 +1,13 @@
 function menuFunction() {
-    var x = document.getElementById("downloads");
+    var x = document.getElementById("menu-div");
     const rolling = document.querySelector(".stop-scrolling");
-    const ActiveHamb = document.querySelectorAll(".hamburguer-icon__bar");
     if (x.className.includes('menu-in')) {
       x.classList.add('menu-out');
       x.classList.remove('menu-in');
-      rolling.style.overflowY = "auto";
-      document.getElementById('hamb-phone').src="img/hamb.svg";
-      for (const box of ActiveHamb) {
-        box.classList.remove('hamburguer-icon__active');
-      }      
+      rolling.style.overflowY = "auto";  
     } else {
       x.classList.add('menu-in');
       x.classList.remove('menu-out');
-      document.getElementById('hamb-phone').src="img/cross.svg";
-      for (const box of ActiveHamb) {
-        box.classList.add('hamburguer-icon__active');
-      }
       rolling.style.overflowY = "hidden";
     }
   }
