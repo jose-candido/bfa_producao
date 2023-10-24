d0a204b45eb03215cc7ac9c0acbf05b52f90d729
jose-candido <jose.pedro.martins.candido@ist.utl.pt>
2023-10-24 23:53:29 +0100

    Links + Arrow visible

--- js/menuOpen.js
+++ js/menuOpen.js
@@ -12,3 +12,33 @@ function menuFunction() {
     }
   }
 
+
+  window.addEventListener("scroll", function() {
+    var landingPage = document.querySelector(".section_1");
+    var minhaDivFixa = document.querySelector(".section_10-arrow");
+    var minhaDivFixa2 = document.querySelector(".arrow-desktop");
+  
+    if (landingPage && window.scrollY < landingPage.offsetHeight) {
+      minhaDivFixa.style.display = "none";
+      minhaDivFixa2.style.opacity = "0";
+    } else {
+      minhaDivFixa.style.display = "block";
+      minhaDivFixa2.style.opacity = "1";
+    }
+  });
+
+  window.addEventListener("scroll", function() {
+    var landingPageM = document.getElementById("home_");
+    var minhaDivFixaM = document.querySelector(".section_10-arrow_phone");
+  
+    if (landingPageM && window.scrollY < landingPageM.offsetHeight) {
+      minhaDivFixaM.style.display = "none";
+    } else {
+      minhaDivFixaM.style.display = "block";
+      minhaDivFixaM.style.opacity = "1";
+    }
+  });
+
+
+
+
